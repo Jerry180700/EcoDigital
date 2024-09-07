ce3d08bfe4f4184a0aa8ed8a5c4728a259610038
Gerardo Gutierrez Velasco <g.gutierrez@lainter.edu.mx>
2024-09-06 18:29:19 -0600

    Se configuran las rutas para las publicaciones y para la API

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  resources :posts
+
+  root to: 'posts#index'
+
+  # API's routes
+  namespace :api do
+    resources :posts, only: %i[index show create update destroy]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
